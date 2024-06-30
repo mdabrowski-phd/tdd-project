439ca93f32058481162c3823aff61f30f9667565
mdabrowski-phd <micdab90@gmail.com>
2024-06-30 17:48:45 +0200

    python: konwersja walut przy użyciu zdefiniowanego kursu wymiany

--- py/test_money.py
+++ py/test_money.py
@@ -18,6 +18,7 @@ class TestMoney(unittest.TestCase):
         fiveDollars = Money(5, "USD")
         tenDollars = Money(10, "USD")
         fifteenDollars = Money(15, "USD")
+        
         portfolio = Portfolio()
         portfolio.add(fiveDollars, tenDollars)
         self.assertEqual(fifteenDollars, portfolio.evaluate("USD"))
@@ -25,10 +26,24 @@ class TestMoney(unittest.TestCase):
     def testAdditionOfDollarsAndEuros(self):
         fiveDollars = Money(5, "USD")
         tenEuros = Money(10, "EUR")
+        
         portfolio = Portfolio()
         portfolio.add(fiveDollars, tenEuros)
+        
         expectedValue = Money(17, "USD")
         actualValue = portfolio.evaluate("USD")
+        self.assertEqual(expectedValue, actualValue,
+                         "%s != %s" % (expectedValue, actualValue))
+        
+    def testAdditionOfDollarsAndWons(self):
+        oneDollar = Money(1, "USD")
+        elevenHundredWons = Money(1100, "KRW")
+        
+        portfolio = Portfolio()
+        portfolio.add(oneDollar, elevenHundredWons)
+        
+        expectedValue = Money(2200, "KRW")
+        actualValue = portfolio.evaluate("KRW")
         self.assertEqual(expectedValue, actualValue,
                          "%s != %s" % (expectedValue, actualValue))
 
