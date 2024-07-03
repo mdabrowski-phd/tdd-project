88de57a7de48cd7092a6ba8f0550c490c11cf021
Michal Dabrowski <micdab90@gmail.com>
2024-07-03 17:31:04 +0200

    python: poprawienie obsługi błędów dla brakujących kursów wymiany

--- py/test_money.py
+++ py/test_money.py
@@ -46,6 +46,19 @@ class TestMoney(unittest.TestCase):
         actualValue = portfolio.evaluate("KRW")
         self.assertEqual(expectedValue, actualValue,
                          "%s != %s" % (expectedValue, actualValue))
+        
+    def testAdditionWithMultipleMissingExchangeRates(self):
+        oneDollar = Money(1, "USD")
+        oneEuro = Money(1, "EUR")
+        oneWon = Money(1, "KRW")
+
+        portfolio = Portfolio()
+        portfolio.add(oneDollar, oneEuro, oneWon)
+        with self.assertRaisesRegex(
+            Exception,
+            "Brakuje kursu \(kursów\) wymiany: \[ USD->Kalganid, EUR->Kalganid, KRW->Kalganid \]"
+            ):
+            portfolio.evaluate("Kalganid")
 
 
 if __name__ == '__main__':
