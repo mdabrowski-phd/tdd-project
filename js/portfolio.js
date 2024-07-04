53ea9db7103709040efa51a039373f19e0f94020
Michal Dabrowski <micdab90@gmail.com>
2024-07-04 18:41:45 +0200

    javaScript: dodanie Bank i refaktoryzacja Portfolio, by używało Bank

--- js/portfolio.js
+++ js/portfolio.js
@@ -10,37 +10,23 @@ class Portfolio {
       this.moneys = this.moneys.concat(moneys);
     }
   
-    evaluate(currency) {
+    evaluate(bank, currency) {
       let failures = [];
       let total = this.moneys.reduce( (sum, money) => {
-        let convertedAmount = this.convert(money, currency);
-        if (convertedAmount === undefined) {
-          failures.push(money.currency + "->" + currency);
-          return sum;
+        try {
+          let convertedMoney = bank.convert(money, currency);
+          return sum + convertedMoney.amount;
         }
-        return sum + convertedAmount;
+        catch (error) {
+          failures.push(error.message);
+          return sum;
+        } 
       }, 0);
 
       if(!failures.length) {
         return new Money(total, currency);
       }
       throw new Error("Brakuje kursu (kursów) wymiany: [ " + failures.join(", ") + " ]");
-    }
-
-    convert(money, currency) {
-      let exchangeRates = new Map();
-      exchangeRates.set("EUR->USD", 1.2);
-      exchangeRates.set("USD->KRW", 1100);
-
-      if (money.currency === currency) {
-        return money.amount;
-      }
-      let key = money.currency + "->" + currency;
-      let rate = exchangeRates.get(key);
-      if (rate === undefined) {
-        return undefined;
-      }
-      return money.amount * rate;
     }
   }
 
--- js/test_money.js
+++ js/test_money.js
@@ -1,8 +1,15 @@
 const assert = require("assert");
 const Money = require("./money");
 const Portfolio = require("./portfolio");
+const Bank = require("./bank");
 
 class MoneyTest {
+  constructor() {
+    this.bank = new Bank();
+    this.bank.addExchangeRate("EUR", "USD", 1.2);
+    this.bank.addExchangeRate("USD", "KRW", 1100);
+  }
+
   testMultiplication() {
     let tenEuros = new Money(10, "EUR");
     let twentyEuros = new Money(20, "EUR");
@@ -22,7 +29,7 @@ class MoneyTest {
 
     let portfolio = new Portfolio();
     portfolio.add(fiveDollars, tenDollars);
-    assert.deepStrictEqual(portfolio.evaluate("USD"), fifteenDollars);
+    assert.deepStrictEqual(portfolio.evaluate(this.bank, "USD"), fifteenDollars);
   }
 
   testAdditionOfDollarsAndEuros() {
@@ -32,7 +39,7 @@ class MoneyTest {
     let portfolio = new Portfolio();
     portfolio.add(fiveDollars, tenEuros);
     let expectedValue = new Money(17, "USD");
-    assert.deepStrictEqual(portfolio.evaluate("USD"), expectedValue);
+    assert.deepStrictEqual(portfolio.evaluate(this.bank, "USD"), expectedValue);
   }
 
   testAdditionOfDollarsAndWons() {
@@ -42,7 +49,7 @@ class MoneyTest {
     let portfolio = new Portfolio();
     portfolio.add(oneDollar, elevenHundredWons);
     let expectedValue = new Money(2200, "KRW");
-    assert.deepStrictEqual(portfolio.evaluate("KRW"), expectedValue);
+    assert.deepStrictEqual(portfolio.evaluate(this.bank, "KRW"), expectedValue);
   }
 
   testAdditionWithMultipleMissingExchangeRates() {
@@ -53,7 +60,18 @@ class MoneyTest {
     let portfolio = new Portfolio();
     portfolio.add(oneDollar, oneEuro, oneWon);
     let expectedError = new Error("Brakuje kursu (kursów) wymiany: [ USD->Kalganid, EUR->Kalganid, KRW->Kalganid ]");
-    assert.throws(function() {portfolio.evaluate("Kalganid")}, expectedError);
+    assert.throws(() => portfolio.evaluate(this.bank, "Kalganid"), expectedError);
+  }
+
+  testConversion() {
+    let tenEuros = new Money(10, "EUR");
+    assert.deepStrictEqual(this.bank.convert(tenEuros, "USD"), new Money(12, "USD"));
+  }
+
+  testConversionWithMissingExchangeRate() {
+    let tenEuros = new Money(10, "EUR");
+    let expectedError = new Error("EUR->Kalganid");
+    assert.throws(() => this.bank.convert(tenEuros, "Kalganid"), expectedError);
   }
 
   runAllTests() {
